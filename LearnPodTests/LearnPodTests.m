3ec83b16770b776545df47d8edebc0b7ce1fa134
oujinfu <oujinfuchn@163.com>
2018-02-26 17:52:06 +0800

    day01

---
+++ LearnPodTests/LearnPodTests.m
@@ -0,0 +1,39 @@
+//
+//  LearnPodTests.m
+//  LearnPodTests
+//
+//  Created by Edison on 2018/2/26.
+//  Copyright © 2018年 Edison. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LearnPodTests : XCTestCase
+
+@end
+
+@implementation LearnPodTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
